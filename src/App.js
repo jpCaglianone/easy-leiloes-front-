c77de82ad2c0dad3127f23d4399269cc89401cde
unknown <joao.caglianone@gmail.com>
2024-02-12 15:25:22 -0300

    subida teste register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header/header';
 import MyCarousel from './components/home/carousel';
 import About from './components/home/about'; 
+import Register from './components/auth/register'; 
 //import Login from './login';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <MyCarousel/>
       <About/>
+      <Register/>
     </div>
   );
 }
---
+++ src/components/auth/register.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../../css/styles.css';
+
+const Register = () => {
+
+    useEffect(() => {
+        $(document).ready(() => {
+            $("#btn-registrar").on("click", (event) => {
+                event.preventDefault();
+                alert("teste ok!");
+            })
+        })
+    }, []);
+
+
+    return (
+        <div className="container">
+            <h2>Registrar-se</h2>
+            <div className='col-4 mx-auto'>
+                <form>
+
+                    <div className="form-group">
+                        <label>Nome completo:</label>
+                        <input type="text" className="form-control" id='email' required />
+                    </div>
+                    <div className="form-group">
+                        <label>Data Nascimento:</label>
+                        <input type="date" className="form-control" id='email' required />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="email" className="form-control" id='email' required />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirme o email:</label>
+                        <input type="email" className="form-control" id='cEmail' required />
+                    </div>
+                    <div className="form-group">
+                        <label>Senha:</label>
+                        <input type="password" className="form-control" id='password' />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirme a senha:</label>
+                        <input type="password" className="form-control" id='cPassword' required />
+                    </div>
+                    <button type="submit" className="btn btn-primary" id="btn-registrar">Registrar</button>
+
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
