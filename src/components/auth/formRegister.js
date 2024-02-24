ba81f2230a586ef474dc8f23617399f04419510c
unknown <joao.caglianone@gmail.com>
2024-02-24 02:57:33 -0300

    inicio do envio de dados de registro para a api de teste

--- src/components/auth/formRegister.js
+++ src/components/auth/formRegister.js
@@ -1,8 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
 const FormRegister = () => {
+
+
+    let url = "https://a5436e77-1d77-4583-8d79-1a0ec14f8b4a-00-13osuyguj3pdp.janeway.replit.dev/";
+
+
+
+    const [formData, setFormData] = useState({
+        name: "",
+        birthday: "",
+        email: "",
+        confirmEmail: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    useEffect(()=>{
+        let btnRegister = document.getElementById("btn-register")
+        btnRegister.addEventListener('click', ()=>{
+            fetch(`${url}register`,{
+            method:'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(formData)
+        })
+        .then(resposta => {resposta.json()})
+        .then(dados => console.log(dados))
+        .catch(error => console.error('Erro:', error));
+        })
+        
+
+    },[])
+
+    const sendData = (event) => {
+        let value = event.target.value
+        let id = event.target.id
+
+        setFormData(previousState => {
+           const newState = {...previousState, [id]:value}
+           return newState
+        });
+    }
+
+    const submitFormButton = (event) => {
+        event.preventDefault();
+    }
+
 
     return (
 
-        
+
         <div className="container" id='custom-register'>
             <br></br>
             <h2>Registrar-se</h2>
@@ -11,35 +58,66 @@ const FormRegister = () => {
 
                     <div className="form-group">
                         <label>Nome completo:</label>
-                        <input type="text" className="form-control" id='email' required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            id='name'
+                            onChange={sendData}
+                            required />
                     </div>
                     <div className="form-group">
                         <label>Data Nascimento:</label>
-                        <input type="date" className="form-control" id='email' required />
+                        <input
+                            type="date"
+                            className="form-control"
+                            id='birthday'
+                            onChange={sendData}
+                            required />
                     </div>
                     <div className="form-group">
                         <label>Email:</label>
-                        <input type="email" className="form-control" id='email' required />
+                        <input
+                            type="email"
+                            className="form-control"
+                            id='email'
+                            onChange={sendData}
+                            required />
                     </div>
                     <div className="form-group">
                         <label>Confirme o email:</label>
-                        <input type="email" className="form-control" id='cEmail' required />
+                        <input
+                            type="email"
+                            className="form-control"
+                            id='confirmEmail'
+                            onChange={sendData}
+                            required />
                     </div>
                     <div className="form-group">
                         <label>Senha:</label>
-                        <input type="password" className="form-control" id='password' />
+                        <input
+                            type="password"
+                            className="form-control"
+                            id='password'
+                            onChange={sendData}
+                            required />
                     </div>
                     <div className="form-group">
                         <label>Confirme a senha:</label>
-                        <input type="password" className="form-control" id='cPassword' required />
+                        <input
+                            type="password"
+                            className="form-control"
+                            id='confirmPassword'
+                            onChange={sendData}
+                            required />
                     </div>
-                    <button type="submit" className="btn btn-primary" id="btn-registrar">Registrar</button>
-                    
+                    <button type="submit" className="btn btn-primary" id="btn-register" onClick={submitFormButton}>Registrar</button>
+
                 </form>
                 <br></br>
             </div>
         </div>
     )
+
 }
 
 export default FormRegister;
--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,29 +1,16 @@
-import React, { useEffect } from 'react';
-import $ from 'jquery';
+
 import '../../css/styles.css';
 import Top from '../header/top';
-import FormRegister from './formRegister'
+import FormRegister from './formRegister';
+
 
 const Register = () => {
-
-    useEffect(() => {
-        window.$ = $;
-        window.jQuery = $;
-
-        $(document).ready(() => {
-            $("#btn-registrar").on("click", (event) => {
-                event.preventDefault();
-                alert("teste ok!");
-            });
-        });
-    }, []);
-
-
+    
     return (
-
         <>
             <Top />
-            <FormRegister />
+            <FormRegister/>
+            
         </>
     );
 };
